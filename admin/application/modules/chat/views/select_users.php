7f90a2f12a33ff610ccac5f074541227fa62c89e
nutthapol-chue <58501595+nutthapol-chue@users.noreply.github.com>
2020-11-07 10:16:48 +0700

    fdf

--- admin/application/modules/chat/views/select_users.php
+++ admin/application/modules/chat/views/select_users.php
@@ -1,8 +1,11 @@
 <div class="col-12">
     <?php foreach ($users as $user) { ?>
+        <?php $count =  $this->chat->getCommentCount($user['id']); ?>
         <button class="btn btn-success mx-auto mb-2 text-white" onclick="chatFrom(<?= $user['id'] ?>)">
             <i class="mdi mdi-account"></i> <?= $user['first_name'] . " " . $user['last_name']; ?>
-            <span class="badge badge-pill badge-danger"><?= $this->chat->getCommentCount($user['id']); ?> </span>
+            <?php if($count > 0){ ?>
+            <span class="badge badge-pill badge-danger"><?= $count ?> </span>
+            <?php } ?>
         </button>
     <?php } ?>
 </div>
